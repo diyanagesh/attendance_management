bb189bb97cedff783f2a1a66bf4a2aab352fe6a8
diyanagesh <diyanagesh02@gmail.com>
2024-12-30 20:55:04 +0530

    Add files via upload

---
+++ Streamlit/app.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import Streamlit as st
+# Generate random geographical data
+df = pd.DataFrame(
+    np.random.randn(500, 2) / [50, 50] + [37.76, -  122.4],
+    columns=['lat', 'lon']
+)
+
+# plot = plt.plot(x, y)
+# st.pyplot(plot)
+# Display the map
+st.map(df)
---
+++ py1.py
@@ -0,0 +1,6 @@
+import os
+
+# Get the directory where the current script is located
+script_directory = os.path.dirname(os.path.abspath(__file__))
+
+print("Directory of the script:", script_directory)
